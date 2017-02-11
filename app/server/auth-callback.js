8ce89922e04d7782ac101f0c99c07b5e05960320
Osman Zupcic <osmanzupcic@gmail.com>
2017-02-11 11:45:08 +0100

    make it better

--- app/server/auth-callback.js
+++ app/server/auth-callback.js
@@ -7,9 +7,5 @@ exports.index = function(req, res){
   /*console.log('auth-callback,user', req.user);
   console.log(JSON.stringify(req.user));*/
   //res.render('auth-callback', { user: JSON.stringify(req.user) });
-	res.render('home2', {
-				title : 'Social Scheduler',
-				udata : req.session.user,
-				showsignin: false
-			});
+	res.redirect('/home2');
 };
